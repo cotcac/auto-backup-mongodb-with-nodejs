800aea97421461d064a7aa0c38a3512b64c25d15
Tran Vuong <cotcac@gmail.com>
2019-10-25 10:15:08 +0700

    update backup with uri

--- backup.js
+++ backup.js
@@ -8,6 +8,8 @@ module.exports = function(time="0 2 * * *"){
         schedule.scheduleJob(time, function(){ 
             cmd.get(
                 `mongodump --host=localhost --port=27017 --db=${process.env.DB}  --out=./backup`,
+                // or you can use the uri ex: db_uri=mongodb://localhost:27017/posts
+                // `mongodump --uri=${process.env.db_uri}  --out=./backup`,
                 function (err, data, stderr) {
                     if (err) return rej("Backup Error");
                     return res("BACKUP SUCCESS AT " + new Date());
